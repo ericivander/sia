afe0bda91ffb5c4fd904eede42da69f0f096530a
Ramadhan Rosihadi Perdana <ramadhanrperdana@gmail.com>
2015-08-22 10:07:12 -0700

    Create All View
    Menyiapkan halaman view

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -1,14 +1,40 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Site extends MY_Controller {
+class Site extends CI_Controller {
 	// load halaman view
-	public function login(){
+	public function index()
+	{
+		$this->load->view('Home');
+	}
+	public function home(){
 		$user = array();
 		//function authentication 
 		//masuk halaman login
-		$this->load->view('Login',$user);	
+		$this->load->view('Home',$user);	
+	}
+	public function main(){
+		$user = array();
+		//function authentication 
+		//masuk halaman login
+		$this->load->view('Main',$user);	
+	}
+	public function profile(){
+		$user = array();
+		//function authentication 
+		//masuk halaman login
+		$this->load->view('Profile',$user);	
+	}
+	public function editmyprofile(){
+		$user = array();
+		//function authentication 
+		//masuk halaman login
+		$this->load->view('Editmyprofile',$user);	
+	}
+	public function setting(){
+		$user = array();
+		//function authentication 
+		//masuk halaman login
+		$this->load->view('Setting',$user);	
 	}
 }
 
